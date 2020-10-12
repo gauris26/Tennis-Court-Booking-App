301e8cf8e69314b4ef58dd934cf55c57142e0ee0
Gauris Javier <garu_gauris@hotmail.com>
2020-10-12 13:08:49 -0400

    Update constants.dart

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -20,7 +20,7 @@ final kFormatDateHumanRead = DateFormat("hh:mm a");
 
 const kSymmetricHorizontal = const EdgeInsets.symmetric(horizontal: 18);
 
-const kOpenMapWeatherApiKey = "1d38bb57edef07e9bb61575b29d95c1b";
+const kOpenMapWeatherApiKey = "Here your Open Map Key";
 
 const kOpenMapWeatherUrl =
     "https://api.openweathermap.org/data/2.5/onecall?lat=$kCourtLatitude&lon=$kCourtLongitude&appid=$kOpenMapWeatherApiKey&units=metric&exclude=current,minutely,hourly,alerts";
